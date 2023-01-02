e16a6eea5baea857da279e63b01a8051a38859ae
pearlbob <bob@bsteele.com>
2023-01-02 14:41:55 -0800

    8008 and middleware struggle

--- bin/server.dart
+++ bin/server.dart
@@ -1,6 +1,7 @@
+import 'dart:developer';
 import 'dart:io';
 
-// import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
 // import 'package:shelf_router/shelf_router.dart';
@@ -20,9 +21,17 @@ import 'package:shelf_web_socket/shelf_web_socket.dart';
 //   return Response.ok('$message\n');
 // }
 
+void _appLogger(String msg, bool isError) {
+  if (isError) {
+    print('[ERROR] $msg');
+  } else {
+    print('my logger: "$msg"');
+  }
+}
+
 void main(List<String> args) async {
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8088');
+  final port = int.parse(Platform.environment['PORT'] ?? '8008');
 
   // // Use any available host or container IP (usually `0.0.0.0`).
   // final ip = InternetAddress.anyIPv4;
@@ -34,15 +43,28 @@ void main(List<String> args) async {
   // final server = await serve(handler, ip, port);
   // print('Server listening on port ${server.port}');
 
-  var handler = webSocketHandler((webSocket) {
+  var handler = webSocketHandler((webSocket) async {
     webSocket.stream.listen((message) {
       webSocket.sink.add("echo $message");
       print('heard message: $message');
     });
   });
 
+  // final handler = const shelf.Pipeline()
+  //     .addMiddleware(corsHeaders())
+  //     .addMiddleware(shelf.logRequests(
+  //     logger: (message, isError) =>
+  //         _logRequest(message, isError: isError)))
+  //     .addHandler((req) async {
+  //   final res = await Router().call(req);
+  //   return res;
+  // });
+
+  // Configure a pipeline that logs requests.
+  final socketServer = const Pipeline().addMiddleware(logRequests()).addHandler(handler);
+
   String wsUrl = '';
-  await shelf_io.serve(handler, 'localhost', port).then((server) {
+  await shelf_io.serve(socketServer, 'localhost', port).then((server) async {
     wsUrl = 'ws://${server.address.host}:${server.port}';
     print('Serving at $wsUrl');
   });
